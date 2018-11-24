a27e618a8baaa2e7a1a4398ce6649c1cd1d06863
张学政 <tseug@263.net>
2018-11-24 21:05:47 +0800

    M5Stack Hello World

---
+++ HelloWorld/src/main.cpp
@@ -0,0 +1,13 @@
+#include <M5Stack.h>
+
+void setup() {
+  // 初始化M5Stack
+  M5.begin();
+
+  // LCD 显示
+  M5.Lcd.print("Hello World");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
