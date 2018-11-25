08a82695c0aa5218c17af603a28e1dafd5cd96cd
张学政 <tseug@263.net>
2018-11-25 22:34:57 +0800

    v0.2

--- HelloWorld/src/main.cpp
+++ HelloWorld/src/main.cpp
@@ -1,17 +1,37 @@
 #include <M5Stack.h>
+#include <WiFi.h>
+
+String wifi_ssid = "BOCOITS";
+String wifi_password = "boco85308686";
+
 
 void setup() {
+  int count = 0;
+
   // 初始化M5Stack
   M5.begin();
-
-  // LCD 显示
-  M5.Lcd.setCursor(100, 120);
-
   M5.Lcd.setTextColor(RED);
-
   M5.Lcd.setTextSize(2);
 
-  M5.Lcd.printf("Hello world!");
+  // 初始化WIFI
+  WiFi.begin(wifi_ssid.c_str(), wifi_password.c_str());
+  
+  M5.Lcd.setCursor(100,100);
+  M5.Lcd.print(wifi_ssid);
+
+  M5.Lcd.setCursor(100, 120);
+  M5.Lcd.print(".");
+  
+  while ( count < 30 ) {
+    if (WiFi.status() == WL_CONNECTED) {
+      M5.Lcd.setCursor(100, 120);
+      M5.Lcd.println("Connected!");
+    }
+
+    delay(500);
+    count++;
+  }
+
 }
 
 void loop() {
